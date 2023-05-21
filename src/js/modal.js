c8bdeb0e6c99f9997d704606defacf32a8399627
michalmazurdev <m.mazur40@gmail.com>
2023-05-22 01:50:32 +0200

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,6 @@
 const posterEl = document.querySelector('#poster_path');
-
 const modalEl = document.querySelector('.modal__backdrop');
-
 const closeButtonEl = document.querySelector('.modal__close');
-
-const teamLinkEl = document.querySelector('.footer-item__link');
-
-const modalTeamEl = document.querySelector('.modal-team__backdrop');
-const closeButtonTeamEl = document.querySelector('.modal-team__close');
-
-function showOrClose() {
-  modalEl.classList.toggle('modal__hidden');
-}
 
 window.addEventListener('click', event => {
   if (event.target.className !== 'movie-card__poster') {
@@ -19,9 +8,14 @@ window.addEventListener('click', event => {
   }
   modalEl.classList.toggle('modal__hidden');
 });
+
 closeButtonEl.addEventListener('click', () => {
   modalEl.classList.toggle('modal__hidden');
 });
+
+const teamLinkEl = document.querySelector('.footer-item__link');
+const modalTeamEl = document.querySelector('.modal-team__backdrop');
+const closeButtonTeamEl = document.querySelector('.modal-team__close');
 
 teamLinkEl.addEventListener('click', event => {
   event.preventDefault();
@@ -30,3 +24,7 @@ teamLinkEl.addEventListener('click', event => {
 closeButtonTeamEl.addEventListener('click', () => {
   modalTeamEl.classList.toggle('modal-team__hidden');
 });
+
+// function showOrClose() {
+//   modalEl.classList.toggle('modal__hidden');
+// }
