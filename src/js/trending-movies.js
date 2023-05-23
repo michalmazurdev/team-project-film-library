a499e2d0b623a6f515283ab0d9b7fe4733ad4764
michalmazurdev <m.mazur40@gmail.com>
2023-05-23 12:27:48 +0200

    fetching developped

--- src/js/trending-movies.js
+++ src/js/trending-movies.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
-// const language = 'en-US';
+
+const searchFormEl = document.getElementById('searchForm');
+const inputEl = document.querySelector('.searchInput');
+const movieListEl = document.querySelector('.movie-list');
+const thisWeekMovieURL = `https://api.themoviedb.org/3/trending/movie/week?`;
+const searchMovieURL = `https://api.themoviedb.org/3/search/movie?`;
+const searchAllURL = `https://api.themoviedb.org/3/search/multi?`;
+const searchPersonURL = `https://api.themoviedb.org/3/search/person?`;
+const searchSeriesURL = `https://api.themoviedb.org/3/search/tv?`;
+
+const language = 'en-US';
 let page = 1;
 let fetchedMovies = [];
-
-// const searchParams = new URLSearchParams({
-//   api_key: '5e58d3162f5aafaf855cf7d900bbc361',
-//   include_adult: false,
-//   language: language,
-//   page: page,
-// });
 
 const fetchTrendingMovies = async page => {
   try {
@@ -25,7 +28,43 @@ const fetchTrendingMovies = async page => {
   }
 };
 
-const drawTrendingMovies = arrayOfMovies => {
+// LOGIKA BUDOWANIA ADRESU URL - ZMIENNEGO ZALEŻNIE OD ZAPYTANIA
+const getURL = () => {
+  const searchParams = new URLSearchParams({
+    query: inputEl.value,
+    api_key: '5e58d3162f5aafaf855cf7d900bbc361',
+    include_adult: false,
+    language: language,
+    page: page,
+  });
+
+  let url;
+  if (inputEl.value === '') {
+    url = `${thisWeekMovieURL}${searchParams}`;
+    console.log('this week');
+  } else {
+    url = `${searchMovieURL}${searchParams}`;
+    console.log('search for movies');
+  }
+  console.log(url);
+  return url;
+};
+
+// FUNKCJA POBIERAJĄCA DANE Z SERWERA W ZALEŻNOŚCI OD WART URL
+const fetchSearchedMovies = async () => {
+  try {
+    const response = await axios.get(getURL());
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  }
+};
+
+const drawMovies = arrayOfMovies => {
   let markup = '';
   let id = 0;
   arrayOfMovies.results.forEach(movie => {
@@ -40,21 +79,53 @@ const drawTrendingMovies = arrayOfMovies => {
         <span class="movie-card__genre" id="genre_ids">Lorem impsum |</span>
         <span class="movie-card__release-date" id="release_date"> ${movie.release_date}</span>
         <!-- in JS need to add a script for changing visibility on Homepage: document.querySelector('.movie-card__rating').classList.add('is-hidden') -->
-        <span class="movie-card__rating" id="vote_average">9.5</span>
+        <span class="movie-card__rating" id="vote_average">${movie.vote_average}</span>
     </div>
 </div>`;
   });
   return markup;
 };
 
-const loadTrendingMovies = markup => {
-  const movieList = document.querySelector('.movie-list');
-  movieList.innerHTML = '';
-  movieList.innerHTML = markup;
+const loadMovies = markup => {
+  movieListEl.innerHTML = '';
+  movieListEl.innerHTML = markup;
 };
 
 window.addEventListener('load', async () => {
   const movies = await fetchTrendingMovies(page);
-  const markup = drawTrendingMovies(movies);
-  loadTrendingMovies(markup);
+  const markup = drawMovies(movies);
+  loadMovies(markup);
 });
+
+searchFormEl.addEventListener('submit', async event => {
+  event.preventDefault();
+  const movies = await fetchSearchedMovies(page);
+  const markup = drawMovies(movies);
+  loadMovies(markup);
+});
+
+// PIERWSZE WYWOŁANIE FUNKCJI DO GENEROWALNIA FILMÓW TYGODNIA
+
+// const showMovies = movies => {
+//   //   movieArray.push(movies);
+//   //   console.log(movies[1].vote_average);
+//   console.log(movies);
+//   movieListEl.innerHTML = movies
+//     .map(movieCard => {
+//       return `<div class="movie-card" id="1">
+//         <img class="movie-card__poster" id="poster_path"
+//         src= 'https://image.tmdb.org/t/p/w500/${movieCard.poster_path}'
+//         alt='${movieCard.title}'
+//         />
+//       <div class="movie-card__figcaption">
+//         <p class="movie-card__title" id="title">${movieCard.title}</p>
+//         <span class="movie-card__genre" id="genre_ids">${movieCard.genre_ids}</span>
+//         <span class="movie-card__release-date" id="release_date">${movieCard.relese_date}</span>
+//         <span class="movie-card__rating" id="vote_average">${movieCard.vote_average}</span>
+//       </div>
+//     </div>`;
+//     })
+//     .join('');
+// };
+
+// fetchMovies();
