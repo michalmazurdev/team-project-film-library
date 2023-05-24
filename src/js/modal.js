accb73e285e5926948ba5056b6ee653eb1e9b10f
michalmazurdev <m.mazur40@gmail.com>
2023-05-24 02:40:00 +0200

    modal.js cleaned

--- src/js/modal.js
+++ src/js/modal.js
@@ -63,19 +63,3 @@ closeButtonEl.addEventListener('click', () => {
   modalEl.classList.toggle('modal__hidden');
   modalContainerEl.innerHTML = '';
 });
-
-// const teamLinkEl = document.querySelector('.footer-item__link');
-// const modalTeamEl = document.querySelector('.modal-team__backdrop');
-// const closeButtonTeamEl = document.querySelector('.modal-team__close');
-
-// teamLinkEl.addEventListener('click', event => {
-//   event.preventDefault();
-//   modalTeamEl.classList.toggle('modal-team__hidden');
-// });
-// closeButtonTeamEl.addEventListener('click', () => {
-//   modalTeamEl.classList.toggle('modal-team__hidden');
-// });
-
-// function showOrClose() {
-//   modalEl.classList.toggle('modal__hidden');
-// }
