8d76460b7bea06abe3765725e8b5dc09ef01a3cf
michalmazurdev <m.mazur40@gmail.com>
2023-05-25 03:38:28 +0200

    modal reorganize

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,7 +3,10 @@ import { movieTypes } from './genres.js';
 const posterEl = document.querySelector('#poster_path');
 const modalEl = document.querySelector('.modal__backdrop');
 const closeButtonEl = document.querySelector('.modal__close');
-const modalContainerEl = document.querySelector('.modal__container');
+
+const formatRate = rating => {
+  return rating.toFixed(1);
+};
 
 window.addEventListener('click', event => {
   if (event.target.className !== 'movie-card__poster') {
@@ -21,45 +24,30 @@ window.addEventListener('click', event => {
     ? `https://image.tmdb.org/t/p/w780${clickedMovie.poster_path}`
     : `https://www.csaff.org/wp-content/uploads/csaff-no-poster.jpg`;
 
-  console.log(modalContainerEl);
-  modalContainerEl.innerHTML = `
-   <div class="modal__poster-container">
-  <img class="modal__poster"
-    src="${posterUrl}"
-    srcset="${posterUrl} 1x, ${posterUrlRetina} 2x"
-    alt=""
-    />
-   </div>
-   <div class="modal__movie-info">
-     <h2 class="modal__title">${clickedMovie.title}</h2>
-     <dl>
-       <dt>Vote / Votes</dt>
-       <dd>
-         <span class="modal__rating">${clickedMovie.vote_average}</span> /
-         <span class="modal__rating modal__rating--number-of-votes">${
-           clickedMovie.vote_count
-         }</span>
-       </dd>
-       <br />
-       <dt>Popularity</dt>
-       <dd>${clickedMovie.popularity}</dd>
-       <dt>Original Title</dt>
-       <dd>${clickedMovie.original_title}</dd>
-       <dt>Genre</dt>
-       <dd>${movieTypes(clickedMovie.genre_ids)}</dd>
-     </dl>
-     <h3 class="modal__descripton-heading">ABOUT</h3>
-     <p class="modal__descripton">
-     ${clickedMovie.overview}
-     </p>
-     <div class="modal__buttons-container">
-       <button class="modal__button modal__button--watched">ADD TO WATCHED</button>
-       <button class="modal__button modal__button--queue">ADD TO QUEUE</button>
-     </div>
-   </div>`;
+  let modalPosterEl = document.querySelector('.modal__poster');
+  let titleEl = document.querySelector('.modal__title');
+  let ratingEl = document.querySelector('.modal__rating');
+  let numOfVotesEL = document.querySelector('.modal__rating--number-of-votes');
+  let popularityEl = document.querySelector("dd[data-info='popularity']");
+  let orgtitleEl = document.querySelector("dd[data-info='orgtitle']");
+  let genresEl = document.querySelector("dd[data-info='genres']");
+  let descriptionEl = document.querySelector('.modal__descripton');
+
+  modalPosterEl.src = `${posterUrl}`;
+  posterEl.srcset = `${posterUrl} 1x, ${posterUrlRetina} 2x`;
+  titleEl.textContent = `${clickedMovie.title}`;
+  ratingEl.textContent = `${formatRate(clickedMovie.vote_average)}`;
+  numOfVotesEL.innerText = `${clickedMovie.vote_count}`;
+  popularityEl.innerText = `${clickedMovie.popularity}`;
+  orgtitleEl.innerText = `${clickedMovie.original_title}`;
+  genresEl.innerText = `${movieTypes(clickedMovie.genre_ids)}`;
+  descriptionEl.innerText = `${clickedMovie.overview}`;
+  modalEl.dataset.movieid = `${clickedMovie.id}`;
 });
 
+function closeModal() {
+  modalEl.classList.remove('modal__hidden');
+}
 closeButtonEl.addEventListener('click', () => {
   modalEl.classList.toggle('modal__hidden');
-  modalContainerEl.innerHTML = '';
 });
