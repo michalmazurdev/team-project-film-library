8911a9cbe2d4fc6f914b9a89fe7af8da7785f66b
Adrian Lacheta <adiczq@gmail.com>
2023-05-27 00:11:02 +0200

    Update loader.js

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,3 +1,11 @@
 $(window).on('load', function () {
   $('.loader-wrapper').fadeOut('slow');
 });
+
+export function showLoader() {
+  const preloader = document.getElementById('preloader');
+  preloader.style.display = 'flex';
+  setTimeout(function () {
+    preloader.style.display = 'none';
+  }, 3000);
+}
