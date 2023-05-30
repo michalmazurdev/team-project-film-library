1bfb2df5a2be7069fe5cf81789ccebafd9be52b1
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-05-30 15:44:23 +0200

    fix

--- src/js/modal_team.js
+++ src/js/modal_team.js
@@ -4,14 +4,14 @@ const closeButtonTeamEl = document.querySelector('.modal-team__close');
 
 teamLinkEl.addEventListener('click', event => {
   event.preventDefault();
-  modalTeamEl.classList.toggle('modal-team__hidden');
+  modalTeamEl.style.display = 'block';
+
+  document.addEventListener('keyup', e => {
+    if (e.key === 'Escape') {
+      modalTeamEl.style.display = 'none';
+    }
+  });
 });
 closeButtonTeamEl.addEventListener('click', () => {
-  modalTeamEl.classList.toggle('modal-team__hidden');
+  modalTeamEl.style.display = 'none';
 });
-
-document.addEventListener('keyup', e => {
-  if (e.key === 'Escape') {
-    modalTeamEl.classList.toggle('modal-team__hidden')
-  }
-} )
